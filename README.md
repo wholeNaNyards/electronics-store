08bd94edbe237423a5269ea88bed88e50d95cf2b
Nick Repetti <nickrepetti@gmail.com>
2015-12-06 12:13:04 -0800

    Initial commit of completed work so far - More to come...

---
+++ README.md
@@ -0,0 +1,5 @@
+#Electronics Store
+
+This web app is a fake online store where users can view, rate, and purchase various electronics.
+
+It will be made using AngularJS, Bootstrap, Spring, and MySQL
---
+++ controllers/electronicsStore.js
@@ -0,0 +1,33 @@
+var electronicsStore = angular.module("electronicsStore", []);
+
+electronicsStore.controller("electronicsStoreController", function($scope) {
+	$scope.data = {
+		categories: [
+			{
+				name: 'Televisions'
+			}, {
+				name: 'Video Games'
+			}, {
+				name: 'Audio'
+			}
+		],
+		products: [
+			{ 
+				name: 'Zivio 55" LED TV', 
+				description: 'A product called one with a very long description about something about the product. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 
+				category: 'Category #1', 
+				price: 100 
+			}, { 
+				name: 'Massung 42" LED Smart TV', 
+				description: 'A product called two with a very long description about something about the product. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 
+				category: 'Category #2', 
+				price: 150 
+			}, { 
+				name: 'GL 30" LED TV', 
+				description: 'A product called three with a very long description about something about the product. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 
+				category: 'Category #1', 
+				price: 10 
+			}
+		]
+	};
+});
